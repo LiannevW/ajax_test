204a532a26251d090104e23e9ca3cce2c5fd445b
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2017-11-12 14:20:15 +0100

    new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,10 @@
 class SongsController < ApplicationController
   def show
-    @song = Song.find(params[:id])
+    @song = Song.find
+  end
+
+  def new
+    artist = Artist.find(params[:artist_id])
+    @song = artist.songs.build
   end
 end
