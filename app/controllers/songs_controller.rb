fc60e268db9b86b33acfcf7892ec0a2c702531db
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2017-11-12 16:55:18 +0100

    x

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,25 @@
 class SongsController < ApplicationController
+before_action :set_artist
+
   def show
-    @song = Song.find
+    @song = Artist.song.find(params[:id])
   end
 
   def new
     artist = Artist.find(params[:artist_id])
     @song = artist.songs.build
   end
+
+  def destroy
+    @song = Song.find(params[:id])
+
+    @song.destroy
+    redirect_to artist_path
+  end
+
+private
+
+def set_artist
+  @artist = Artist.find(params[:id])
+end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,22 @@
+
 require 'rails_helper'
 
-RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+Rspce.describe Song, type: :model do
+  describe "validations" do
+    it "is invalid without a name" do
+      song = Song.new(name: "")
+      song.valid?
+      expect(song.errors).to have_key(:name)
+    end
+  end
+
+  describe "associatin with artist" do
+    let(:song) { create :song }
+
+    it "belongs to a artist" do
+      song = artist.songs.build(name: "")
+
+      expect(song.artist). to eq(artist)
+  end
+end
 end
